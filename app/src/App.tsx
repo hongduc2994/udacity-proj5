66e4423a15e35a82abe8f1d0f5075ceb115cfe1a
Ducdao <hongduc2994@gmail.com>
2023-10-15 15:33:41 +0700

    new app version

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -19,14 +19,14 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          THIS LINE IS ALTERED v2 count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        I changed this line to show that new ENV is deployed
       </p>
     </>
   )
